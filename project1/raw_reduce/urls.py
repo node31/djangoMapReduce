57e4ac0bf4e7b3abaf42ae3235f8560e814ba040
node31 <pranavgupta3131@gmail.com>
2014-06-19 10:28:34 +0530

    second commit

--- project1/raw_reduce/urls.py
+++ project1/raw_reduce/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('',
 		url(r'^$',views.index,name = 'index'),
 		url(r'^insert/$',views.insert,name = 'insert'),
 		url(r'^performReduce/$',views.perform_map_reduce,name = 'perform_map_reduce'),
+		url(r'^edit/$',views.edit_page,name = 'edit'),
+		url(r'^edit_object/$',views.edit_object,name = 'edit_object'),
 	)
